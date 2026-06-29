38d024d401f58ce699af0f43a846f096787586cf
krupeshpatil <patilkrupesh48@gmail.com>
2026-06-29 14:52:03 +0530

    adding the files which i created earlier
    this repo was made after i learnt some amount of C programming so now i am just uploading my old files here

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//function declaration
+void printhello();
+
+
+int main()
+{
+	printhello(); //function call
+	printhello();
+	printhello();
+	return 0;
+}
+
+void printhello()
+
+{
+	//function definition
+	printf("hello world\n");
+	printf("stay loyal man !!!\n");
+}
